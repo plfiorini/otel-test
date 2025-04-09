f3fba548b63a1c37eab5ce343934b27a9d4518f8
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2025-04-09 08:01:47 +0200

    Revamp k8s config

--- k8s/install.sh
+++ k8s/install.sh
@@ -4,34 +4,5 @@ set -e
 
 curdir=$(dirname `readlink -f $0`)
 
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-helm repo update
-
-kubectl config use-context kind-otel-test
-
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
-  --namespace monitoring \
-  --create-namespace \
-  --values ${curdir}/prometheus-values.yaml
-
-helm upgrade --install grafana grafana/grafana \
-  --namespace monitoring \
-  --create-namespace \
-  --values ${curdir}/grafana-values.yaml
-
-helm upgrade --install loki grafana/loki-stack \
-  --namespace monitoring \
-  --create-namespace \
-  --values ${curdir}/loki-values.yaml
-
-helm upgrade --install tempo grafana/tempo-distributed \
-  --namespace monitoring \
-  --create-namespace \
-  --values ${curdir}/tempo-values.yaml
-
-helm upgrade --install otel-collector open-telemetry/opentelemetry-collector \
-  --namespace monitoring \
-  --create-namespace \
-  --values ${curdir}/otel-values.yaml
+kubectl apply -k monitoring/
+kubectl apply -k apps/
--- k8s/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-helm uninstall --namespace monitoring otel-collector || true
-helm uninstall --namespace monitoring tempo || true
-helm uninstall --namespace monitoring loki || true
-helm uninstall --namespace monitoring grafana || true
-helm uninstall --namespace monitoring prometheus || true
